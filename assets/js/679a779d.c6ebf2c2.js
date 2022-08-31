5822e94fcdfbe1bab02ad83d51fcc1ead6067f28
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-08-31 14:06:14 -0500

    Deploy website - based on 8ed53e1601573d51499444a1c09f81345b6ecb12

---
+++ assets/js/679a779d.c6ebf2c2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwrangling=self.webpackChunkwrangling||[]).push([[792],{9954:e=>{e.exports=JSON.parse('{"title":"Course Modules","slug":"/modules","permalink":"/modules","navigation":{"next":{"title":"1 - (re)Introduction to Python and Pandas","permalink":"/modules/python-pandas"}}}')}}]);
---
+++ assets/js/bdf21c37.befb3f2e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwrangling=self.webpackChunkwrangling||[]).push([[255],{3962:e=>{e.exports=JSON.parse('{"title":"Syllabus","slug":"/syllabus","permalink":"/syllabus","navigation":{"previous":{"title":"External Resources","permalink":"/resources/external"},"next":{"title":"Calendar","permalink":"/syllabus/calendar"}}}')}}]);
